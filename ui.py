f12edf8803796034d9016e626f454d8e18863efc
sh4lan <sh4lan@gmail.com>
2025-11-28 13:48:46 +0100

    clean up

--- ui.py
+++ ui.py
@@ -1,39 +1,54 @@
-# hello_world.py
-
 import PySimpleGUI4 as sg
 
 image_paths = [f'/home/omahayomaso/Projects/FUN/zespol-wiosna-ai/images_for_ui/KOTEL1_walk{x}.png' for x in range(1,3)]
 
 print(image_paths)
-layout = [
-    [sg.Image(key='img', filename=image_paths[0])],
-    [sg.Button("next", key='next'), sg.Button("prev", key='prev')],
-]
-
-window = sg.Window(title="Hello World", layout=layout, margins=(100, 50))
-
-idx = 0
 
 
-def update_image(idx):
-    window['img'].update(filename=image_paths[idx], visible=True)
-    window.refresh()
+def select_files():
+    layout = [
+        [sg.FolderBrowse(key='img')],
+    ]
 
+    window = sg.Window(title="Select files", layout=layout, margins=(100, 50))
 
-while True:
-    event, values = window.read()
-    print(event)
+    while True:
 
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
+        window.read()
 
-    if event == 'next':
-        idx += 1
-        update_image(idx)
+# select_files() <- nie dziala rn na linuxie
 
-    if event == 'prev':
-        idx -= 1
-        update_image(idx)
+def browse_images(image_paths):
 
-    #text_input = values[0]
-    #sg.popup('You entered', text_input)
+    layout = [
+        [sg.Image(key='img', filename=image_paths[0])],
+        [sg.Button("prev", key='prev'), sg.Button("next", key='next')],
+    ]
+
+    window = sg.Window(title="Results", layout=layout, margins=(100, 50))
+
+    def update_image(idx):
+        window['img'].update(filename=image_paths[idx], visible=True)
+        window.refresh()
+
+    idx = 0
+
+    while True:
+        event, values = window.read()
+        print(event)
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+
+        if event == 'next':
+            idx += 1
+            update_image(idx)
+
+        if event == 'prev':
+            idx -= 1
+            update_image(idx)
+
+        #text_input = values[0]
+        #sg.popup('You entered', text_input)
+
+browse_images(image_paths)
